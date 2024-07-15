c287e4eb349f2ba43a28b4916bd92d919e18f2cb
Mikhail <smisha2104@gmail.com>
2024-07-15 03:09:13 +0300

    Add SQL requests for business scripts

--- src/main/java/com/example/SpringDataBasicDemo/Repo/ExamRepository.java
+++ src/main/java/com/example/SpringDataBasicDemo/Repo/ExamRepository.java
@@ -6,17 +6,4 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ExamRepository extends JpaRepository<Exam, Integer> {
-//    //pilots who don't have contracts
-//    @Query(value = "select p from Pilot p " +
-//            "join p.contract co where size(p.contract) = 0")
-//    List<Pilot> findByPilotIsNull(@Param(value = "contract") Set<Contract> contract);
-//
-//    //pilots who have a certain type of license
-//    @Query(value = "select p from Pilot p " +
-//            "join p.licenseDistribution ld " + "join ld.license l where l.type = :type")
-//    List<Pilot> findPilotByLicense(@Param(value = "type") String type);
-//
-//    //pilots who have penalty points less than the set value
-//    @Query(value = "select p from Pilot p " + "join p.licenseDistribution ld where ld.penaltyPoints <= :penaltyPoints")
-//    List<Pilot> findAllByTypeAndPenaltyPoints(@Param(value = "penaltyPoints") Integer penaltyPoints);
 }
