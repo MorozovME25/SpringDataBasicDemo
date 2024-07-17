c73b6b39351b915b0cb78bc36c19db2913f8d540
Mikhail <smisha2104@gmail.com>
2024-07-18 00:06:20 +0300

    update baserepo

--- src/main/java/com/example/SpringDataBasicDemo/Repo/Impls/BaseRepo.java
+++ src/main/java/com/example/SpringDataBasicDemo/Repo/Impls/BaseRepo.java
@@ -10,28 +10,8 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public abstract class BaseRepo<Entity, EntityID> {
-    protected JpaRepository<Entity, EntityID> repository;
+public abstract class BaseRepo<Entity> {
 
     @PersistenceContext
     protected EntityManager entityManager;
-
-    private Class<Entity> entityClass;
-
-    @Transactional
-    public void save(Entity entity) {
-        repository.save(entity);
-    }
-
-    @Transactional
-    public Entity findById(EntityID id) {
-        Optional<Entity> optionalEntity = repository.findById(id);
-        return optionalEntity.orElseThrow(() ->
-                new EntityNotFoundException(entityClass.getSimpleName() + " with id " + id + " not found."));
-    }
-
-    @Transactional
-    public void update(Entity entity) {
-        entityManager.merge(entity);
-    }
 }
